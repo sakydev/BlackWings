868a2f6e442a0369627992fad8616c82d1c72a76
Saqib Razzaq <saqibrzzaq@gmail.com>
2024-04-01 09:28:36 +0200

    wire repositories

--- internal/wire.go
+++ internal/wire.go
@@ -1,11 +1,13 @@
 package internal
 
 import (
+	"BlackWings/internal/repositories"
 	"BlackWings/internal/services"
 
 	"github.com/samber/do"
 )
 
 func WireDependencies(i *do.Injector) {
+	repositories.Wire(i)
 	services.Wire(i)
 }
