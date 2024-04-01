ebe8a61a4ad29b7ba44118dca0e2282378bebadd
Saqib Razzaq <saqibrzzaq@gmail.com>
2024-04-01 09:28:09 +0200

    connect to correct database

--- database/database.go
+++ database/database.go
@@ -2,17 +2,27 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 )
 
-const databaseFilepath = "database.sqlite"
+const databaseFilename = "database.sqlite"
 
 func GetDatabase() *sql.DB {
-	db, err := sql.Open("sqlite3", databaseFilepath)
+	db, err := sql.Open("sqlite3", getDatabaseFilePath())
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
 	return db
+}
+
+func getDatabaseFilePath() string {
+	parentDirectory, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return fmt.Sprintf("%s/database/%s", parentDirectory, databaseFilename)
 }
