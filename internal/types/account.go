3dfcd4482168033bb17dfd757f0a2bcc44a55fdd
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-04-07 13:21:05 +0200

    update Account type to include new fields

--- internal/types/account.go
+++ internal/types/account.go
@@ -1,9 +1,11 @@
 package types
 
 type Account struct {
-	Name         string `json:"name"`
-	ClientID     string `json:"clientId"`
-	ClientSecret string `json:"clientSecret"`
-	Raw          string `json:"raw"`
-	App          App    `json:"app"`
+	ID              int64  `json:"id"`
+	Name            string `json:"name"`
+	ClientID        string `json:"clientId"`
+	ClientSecret    string `json:"clientSecret"`
+	CredentialsJSON string `json:"CredentialsJson"`
+	TokenJSON       string `json:"tokenJson"`
+	App             App    `json:"app"`
 }
