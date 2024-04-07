28ce989417606b5c426314c6b098b38d3ef63e7b
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-04-07 13:21:22 +0200

    add email type

--- internal/types/email.go
+++ internal/types/email.go
@@ -1,1 +1,14 @@
-package apps
+package types
+
+import (
+	"github.com/guregu/null/v5"
+	"time"
+)
+
+type EmailResponse struct {
+	Subject     string
+	SenderName  null.String
+	SenderEmail string
+	Date        time.Time
+	Snippet     string
+}
